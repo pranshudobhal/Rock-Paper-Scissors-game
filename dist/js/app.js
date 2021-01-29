3fc6a4e722036c558f7a1c4d22e97ca773db1e07
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-01-30 03:21:19 +0530

    completed mobile view

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,20 @@
 const btnRules = document.querySelector('.rules__btn');
 const btnRulesClose = document.querySelector('.close-btn');
+const score = document.querySelector('.score__total');
+const userAnswer = document.querySelectorAll('.user-answer');
+const userDisplayOption = document.querySelector('.user-image');
+const userDisplayOptionDiv = document.querySelector('.user__answer');
+const computerDisplayOption = document.querySelector('.computer-image');
+const computerDisplayOptionDiv = document.querySelector('.computer__answer');
+const mainMenu = document.querySelector('.main-menu');
+const resultMenu = document.querySelector('.result-menu');
+const playAgain = document.querySelector('.play__again');
+const winnerText = document.querySelector('.result');
 
+var items = ['rock', 'paper', 'scissors'];
+score.innerHTML = 0;
+
+//rules modal open and close buttons
 btnRules.addEventListener('click', function () {
   document.querySelector('.rules-modal').style.display = 'block';
 });
@@ -8,3 +22,86 @@ btnRules.addEventListener('click', function () {
 btnRulesClose.addEventListener('click', function () {
   document.querySelector('.rules-modal').style.display = 'none';
 });
+
+userAnswer.forEach((answer) => answer.addEventListener('click', optionSelected));
+
+playAgain.addEventListener('click', resetToMain);
+
+//reset to main menu
+function resetToMain() {
+  mainMenu.style.display = 'block';
+  resultMenu.style.display = 'none';
+  userDisplayOptionDiv.className = '';
+  userDisplayOptionDiv.classList.add('user__answer');
+  computerDisplayOptionDiv.className = '';
+  computerDisplayOptionDiv.classList.add('computer__answer');
+  // mainMenu.style.backgroundImage = "url('./images/bg-triangle.svg')";
+  mainMenu.style.backgroundImage = "url('/images/bg-triangle.svg')";
+}
+
+function optionSelected() {
+  mainMenu.style.display = 'none';
+  mainMenu.style.backgroundImage = 'none';
+  resultMenu.style.display = 'block';
+
+  var userOption = this.id;
+
+  //generate random computer answer
+  var computerOption = items[Math.floor(Math.random() * items.length)];
+
+  if (userOption == 'rock') {
+    createUserOption(userOption, 'bot');
+  }
+  createUserOption(userOption);
+
+  if (computerOption == 'rock') {
+    createComputerOption(computerOption, 'bot');
+  }
+  createComputerOption(computerOption);
+
+  var winner = findWinner(userOption, computerOption);
+  winnerText.innerHTML = winner;
+}
+
+//check for winner
+function findWinner(user, computer) {
+  if (user == computer) {
+    return 'draw';
+  } else if (user == 'paper' && computer == 'rock') {
+    score.innerHTML++;
+    return 'you win';
+  } else if (user == 'paper' && computer == 'scissors') {
+    score.innerHTML--;
+    return 'you lose';
+  } else if (user == 'rock' && computer == 'paper') {
+    score.innerHTML--;
+    return 'you lose';
+  } else if (user == 'rock' && computer == 'scissors') {
+    score.innerHTML++;
+    return 'you win';
+  } else if (user == 'scissors' && computer == 'rock') {
+    score.innerHTML--;
+    return 'you lose';
+  } else if (user == 'scissors' && computer == 'paper') {
+    score.innerHTML++;
+    return 'you win';
+  }
+}
+
+//create user selected option
+function createUserOption(option, pos = 'top') {
+  userDisplayOption.src = `images/icon-${option}.svg`;
+  userDisplayOptionDiv.classList.remove('result-menu');
+  userDisplayOptionDiv.classList.remove(`${pos}__row`);
+  userDisplayOptionDiv.classList.remove('user__answer');
+  userDisplayOptionDiv.classList.add(`${pos}__row--${option}`);
+}
+
+//create computer selected option
+function createComputerOption(option, pos = 'top') {
+  computerDisplayOption.src = `images/icon-${option}.svg`;
+  computerDisplayOptionDiv.classList.remove('result-menu');
+  computerDisplayOptionDiv.classList.remove(`${pos}__row`);
+  computerDisplayOptionDiv.classList.remove('computer__answer');
+  computerDisplayOptionDiv.classList.add(`${pos}__row--${option}`);
+}
